f48c0be9eb3392f68323693b0847024be570cdc9
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-04-29 13:10:44 -0400

    satisfy test parameters

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    const number = Math.floor(Math.random() * 1000);
+    if (num < 0) {
+        return "ERROR";
+    }
+    let string = '';
+    for (let i = 0; i < num; i++) {
+        string += word;
+    }
+    return string;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
