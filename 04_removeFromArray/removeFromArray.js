67fc5789574ea51e34e615d7b9632e3f2971aa6b
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-05-02 18:32:11 -0400

    satisfy all tests besides remove duplicates

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(original, ...toRemove) {
+    //return original.filter(original => (original !== remove));
+    toRemove.forEach((toRemove) => {
+        const index = original.indexOf(toRemove);
+        if (index > -1)
+            original.splice(index,1);
+    });
+    
 
+    return original;
 };
 
 // Do not edit below this line
