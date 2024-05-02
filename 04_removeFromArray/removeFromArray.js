17de0922df400057188964e564cff63acf16d344
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-05-02 18:36:26 -0400

    satisfy all test conditions for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,15 @@
 const removeFromArray = function(original, ...toRemove) {
     //return original.filter(original => (original !== remove));
+    let length = toRemove.length;
+
+    for (i = -1; i < toRemove.length; i++) {
     toRemove.forEach((toRemove) => {
         const index = original.indexOf(toRemove);
         if (index > -1)
             original.splice(index,1);
     });
     
-
+    }
     return original;
 };
 
