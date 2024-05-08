206c3e509dc57527054f17bbf144ed8dd85a1ec0
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-05-08 19:03:38 -0400

    setup if statement to sort input values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(inta,intb) {
+// if else statement to assign start and end to correct ints 
+if (inta <= intb) {
+    let start = inta;
+    let end = intb;
+    } else if (inta > intb) {
+        let start = intb;
+        let end = inta;
+    }else {return "ERROR"
 
+    }
+
+}
+//
 };
 
 // Do not edit below this line
