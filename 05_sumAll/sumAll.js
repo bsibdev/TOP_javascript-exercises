5a941432621623a0a53b9897906513383436ddfb
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-05-10 00:08:27 -0400

    satisfy basic conditions for 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,56 @@
 const sumAll = function(inta,intb) {
-// if else statement to assign start and end to correct ints 
-if (inta <= intb) {
-    let start = inta;
-    let end = intb;
-    } else if (inta > intb) {
-        let start = intb;
-        let end = inta;
-    }else {return "ERROR"
+ 
+let startValue;
+let endValue;
 
+// conditional to assign start and end to correct ints
+  if (inta <= intb && typeof inta == "number" && typeof intb =="number" && inta + intb >=0) {
+    startValue = inta;
+    endValue = intb;
+    } else if (inta > intb && typeof inta == "number" && typeof intb =="number" && inta + intb >=0) {
+        startValue = intb;
+        endValue = inta;
+    } else {
+      return "ERROR"
     }
 
-}
-//
+//create & fill array with values to add
+const range = [];
+
+for (let i = 0; i < endValue; i++) {
+    range.push(i);
+    return range;
 };
+
+//add values of array elements together
+let sum = 0;
+for (let i = 0; i < range.length; i++) {
+  sum += range[i];
+}
+
+return sum;
+
+};
+
+
+//
+  /*test('sums numbers within the range', () => {
+    expect(sumAll(1, 4)).toEqual(10);
+  });
+  test('works with large numbers', () => {
+    expect(sumAll(1, 4000)).toEqual(8002000);
+  });
+  test.skip('works with larger number first', () => {
+    expect(sumAll(123, 1)).toEqual(7626);
+  });
+  test.skip('returns ERROR with negative numbers', () => {
+    expect(sumAll(-10, 4)).toEqual('ERROR');
+  });
+  test.skip('returns ERROR with non-number parameters', () => {
+    expect(sumAll(10, "90")).toEqual('ERROR');
+  });
+  test.skip('returns ERROR with non-number parameters', () => {
+    expect(sumAll(10, [90, 1])).toEqual('ERROR');*/
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,34 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 400 == 0) {
+        return true;
+    } else if (year % 4 == 0) {
+            return true;
+        } else {return false;
+            }
+  };
 
-};
+/*const leapYears = require('./leapYears')
+
+describe('leapYears', () => {
+  test('works with non century years', () => {
+    expect(leapYears(1996)).toBe(true);
+  });
+  test.skip('works with non century years', () => {
+    expect(leapYears(1997)).toBe(false);
+  });
+  test.skip('works with ridiculously futuristic non century years', () => {
+    expect(leapYears(34992)).toBe(true);
+  });
+  test.skip('works with century years', () => {
+    expect(leapYears(1900)).toBe(false);
+  });
+  test.skip('works with century years', () => {
+    expect(leapYears(1600)).toBe(true);
+  });
+  test.skip('works with century years', () => {
+    expect(leapYears(700)).toBe(false);
+  });
+});*/
 
 // Do not edit below this line
 module.exports = leapYears;
