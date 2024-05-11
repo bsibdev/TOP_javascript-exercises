5938e86b2b99439c131c50afa6d1949db821fe33
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-05-11 12:29:55 -0400

    satisfy all test conditions for 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const CONVERSIONFACTOR = 5/9;
+  let celcius = (fahrenheit -32)*CONVERSIONFACTOR;
+  return Math.round(celcius*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const CONVERSIONFACTOR = 9/5;
+  let fahrenheit = (celcius * CONVERSIONFACTOR)+32;
+  return Math.round(fahrenheit*10)/10;
 };
 
 // Do not edit below this line
