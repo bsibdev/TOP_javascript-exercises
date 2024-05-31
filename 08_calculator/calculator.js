5aa43e0daf38dd4db47d847886bc430593f1247f
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-05-31 03:00:53 -0400

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,33 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	let sum = num1 + num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+    let difference = num1 - num2;
+    return difference;
+  };
+
+const sum = function(toAdd) {
+	return toAdd.reduce((total, current) => total + current,0);
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const multiply = function(toProduct) {
+  return toProduct.reduce((total, current) => total * current);
 
 };
 
-const power = function() {
-	
+const power = function(num,pow) {
+	return Math.pow(num,pow);
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num ===0) return 1;
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *=i;
+  }
+  return product;
 	
 };
 
