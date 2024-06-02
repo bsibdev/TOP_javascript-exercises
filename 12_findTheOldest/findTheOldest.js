88d935ef8a29285c819e8492321743304abdbbc3
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-06-02 13:48:58 -0400

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,77 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth,currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
+    
+};  
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    } return death - birth;
+   };
 
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+/*describe('findTheOldest', () => {
+  test('finds the person with the greatest age!', () => {
+    const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+    expect(findTheOldest(people).name).toBe('Ray');
+  });
+  test.skip('finds the oldest person if yearOfDeath field is undefined on a non-oldest person', () => {
+    const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+    expect(findTheOldest(people).name).toBe('Ray');
+  });
+  test.skip('finds the oldest person if yearOfDeath field is undefined for the oldest person', () => {
+    const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1066,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+    expect(findTheOldest(people).name).toBe('Carly');
+  });*/
