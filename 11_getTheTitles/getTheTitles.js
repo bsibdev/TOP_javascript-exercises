f6d0a108ce4630f93d796c6fa9502648d37a03a6
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-06-02 02:21:16 -0400

    complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,27 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    /*const titles = books.map((book) => {
+        return book.title;
+    });
+    return titles;*/
+    //Can be done in one line:
+    return books.map((book) => books.title)
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+/*describe('getTheTitles', () => {
+    const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
+
+  test('gets titles', () => {
+    expect(getTheTitles(books)).toEqual(['Book','Book2']);
+  });*/
